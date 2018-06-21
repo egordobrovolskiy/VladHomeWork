dabace8efb3132fc392d56c6cb05970a5811503b
Egor Dobrovolskiy <egordobrovolskiy@gmail.com>
2018-06-21 17:43:40 +0300

    Task 4 (Library) - refactoring

--- Vlad.HomeWork/src/lesson2/library/LibraryRunner.java
+++ Vlad.HomeWork/src/lesson2/library/LibraryRunner.java
@@ -2,14 +2,8 @@ package lesson2.library;
 
 public class LibraryRunner {
     public static void main(String[] args) {
-        Book mumu = new Book.Builder("Муму", "Иван Тургенев").publisher("Дрофа").dateOfPublication("2010").pages(64).price(13L).typeOfBinding(TypeOfBinding.SOFT_COVER).build();
-        Book otciAndDeti = new Book.Builder("Отцы и дети", "Иван Тургенев").publisher("Омега").dateOfPublication("2016").pages(304).price(99L).typeOfBinding(TypeOfBinding.HARD_COVER).build();
-        Book revizor = new Book.Builder("Ревизор", "Николай Гоголь").publisher("Омега").dateOfPublication("2016").pages(176).price(96L).build();
 
-        Library library = new Library();
-        library.addBook(mumu);
-        library.addBook(otciAndDeti);
-        library.addBook(revizor);
+        Library library = createLibrary();
 
         System.out.println("По автору ======================================================");
         System.out.println(library.getBooksByAuthor("Иван Тургенев"));
@@ -17,5 +11,15 @@ public class LibraryRunner {
         System.out.println(library.getBooksByPublisher("Омега"));
         System.out.println("С даты публикации ==============================================");
         System.out.println(library.getBooksFromDate("2010"));
+    }
+
+    private static Library createLibrary() {
+        Library result = new Library();
+
+        result.addBook(new Book.Builder("Муму", "Иван Тургенев").publisher("Дрофа").dateOfPublication("2010").pages(64).price(13L).typeOfBinding(TypeOfBinding.SOFT_COVER).build());
+        result.addBook(new Book.Builder("Отцы и дети", "Иван Тургенев").publisher("Омега").dateOfPublication("2016").pages(304).price(99L).typeOfBinding(TypeOfBinding.HARD_COVER).build());
+        result.addBook(new Book.Builder("Ревизор", "Николай Гоголь").publisher("Омега").dateOfPublication("2016").pages(176).price(96L).build());
+
+        return result;
     }
 }
